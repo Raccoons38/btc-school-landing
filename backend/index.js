96ad38c9c8d568b391e4f0fd2a761e62ea96795c
Никита Максимов <ruvemaximus@gmail.com>
2023-08-10 23:54:43 +0800

    Добавил серверную часть

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('db.sqlite3');
+
+
+const app = express();
+app.use(express.json());
+
+app.post('/', async (req, res) => {
+    db.serialize(() => {
+        const stmt = db.prepare('INSERT INTO users(name, email, phone, comment) VALUES(?,?,?,?)');
+        stmt.run(req.body.name, req.body.email, req.body.phone, req.body.comment);
+        stmt.finalize();
+    });
+
+    res.json({
+        status: 'ok',
+        message: 'Ваши данные успешно записаны'
+    })
+})
+
+app.listen(3000);
