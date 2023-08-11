e955b6e0b1205ccc4b3922067465d1753c649978
Никита <ruvemaximus@gmail.com>
2023-08-11 11:51:59 +0800

    Связал сервер с клиентом

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
+const cors = require('cors');
+
+
 const db = new sqlite3.Database('db.sqlite3');
-
-
 const app = express();
+
 app.use(express.json());
+app.options('*', cors());
+
 
 app.post('/', async (req, res) => {
+    console.log('Request accepted!');
+
     db.serialize(() => {
         const stmt = db.prepare('INSERT INTO users(name, email, phone, comment) VALUES(?,?,?,?)');
         stmt.run(req.body.name, req.body.email, req.body.phone, req.body.comment);
